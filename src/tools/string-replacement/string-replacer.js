b64edf65cf4bfebc5f6d0371abacb8bff2d13ce1
Kevin <kvissuet@gmail.com>
2019-09-12 21:32:34 -0700

    fixes replacer

--- src/tools/string-replacement/string-replacer.js
+++ src/tools/string-replacement/string-replacer.js
@@ -3,7 +3,7 @@ import _ from 'lodash';
 export default ({input, rules}) => {
   let replacement = input
   _.forEach(rules, ({start, end}) => {
-    replacement = _.replace(replacement, start, end)
+    replacement = _.replace(replacement,new RegExp(start, "g"), end)
   })
   return replacement
 }
